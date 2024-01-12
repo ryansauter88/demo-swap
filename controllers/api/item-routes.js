e5121d7223175a8f1e01dfac7fa5023f613dc04a
Red Knight <127162163+RedKnight88@users.noreply.github.com>
2024-01-12 11:22:59 -0500

    added all initial concept api routes

---
+++ controllers/api/item-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { Item } = require('../../models');
+
+router.get('/:id', async (req, res) => {
+    try {
+        const newItem = await Item.FindbyPk(req.params.id);
+        res.status(200).json(newItem);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
