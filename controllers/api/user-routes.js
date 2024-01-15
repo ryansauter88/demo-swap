87ba3f9b1cbd080570e28cd7a32678285f491ca3
Red Knight <127162163+RedKnight88@users.noreply.github.com>
2024-01-15 14:13:25 -0500

    added get requests for users and trades

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,17 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Trade } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/:id', withAuth, async (req, res) => {
+  try {
+      const newUser = await User.findByPk(req.params.id, {
+        include: [{ model: Trade}]
+      });
+      res.status(200).json(newUser);
+  } catch (err) {
+      res.status(400).json(err);
+  }
+})
 
 router.post('/', async (req, res) => {
   try {
