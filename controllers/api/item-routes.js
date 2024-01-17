979186451de1aa0c6266cc6245e39b7ab5f059ee
Red Knight <127162163+RedKnight88@users.noreply.github.com>
2024-01-16 22:34:33 -0500

    bugfix for item filename

--- controllers/api/item-routes.js
+++ controllers/api/item-routes.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
 const { Item } = require('../../models');
 
+router.get('/', async (req, res) => {
+    try {
+        const allItem = await Item.findAll();
+        res.status(200).json(allItem);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
 router.get('/:id', async (req, res) => {
     try {
         const newItem = await Item.findByPk(req.params.id);
--- models/Item.js
+++ models/Item.js
@@ -24,7 +24,7 @@ Item.init({
     rarity: { 
         type: DataTypes.STRING 
     },
-    img_filename: {
+    filename: {
         type: DataTypes.STRING
     }
 }, {
