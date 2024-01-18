4d4ac0031f1d2f340c2d2a0630aec60a21fef950
Evan-Beck <ebeck703@gmail.com>
2024-01-18 15:29:35 -0500

    Finished README and added comments to various files in views and models.

--- public/js/login.ts
+++ public/js/login.ts
@@ -1,10 +1,13 @@
 const loginForm = document.querySelector('.login-container form');
 const registerForm = document.querySelector('.registration-container form');
+// Selecting the login and registration form elements from the document.
 
-
+// Asynchronous function to handle the login form submission.
 const loginFormHandler = async (event: Event): Promise<void> => {
+     // Prevents the default form submission behavior.
     event.preventDefault();
  
+    // Selecting the email and password input elements from the login form.
     const emailInput = document.querySelector('#email-login') as HTMLInputElement;
     const passwordInput = document.querySelector('#password-login') as HTMLInputElement;
 
@@ -12,13 +15,13 @@ const loginFormHandler = async (event: Event): Promise<void> => {
     // const email = emailInput.value.trim();
     // const password = passwordInput.value.trim();
 
-
+// Checks if both email and password inputs are available.
     if (emailInput && passwordInput) {
         const email = emailInput.value.trim();
     const password = passwordInput.value.trim();
-   
+   // Trimming the values to remove any leading/trailing whitespace
 
-
+ // Checks if both email and password are not empty.
     if (email && password) {
         const response = await fetch('/api/users/login', {
             method: 'POST',
@@ -36,7 +39,7 @@ const loginFormHandler = async (event: Event): Promise<void> => {
 }
 };
 
-
+// Adding an event listener to the login form for the submit event.
 if (loginForm) {
     loginForm.addEventListener('submit', loginFormHandler);
 }
@@ -45,13 +48,13 @@ if (loginForm) {
 const signupFormHandler = async (event: Event): Promise<void> => {
     event.preventDefault();
 
-
+// Selecting the input elements from the registration form.
     const nameInput = document.querySelector('#name-signup') as HTMLInputElement;
     const emailInput = document.querySelector('#email-signup') as HTMLInputElement;
     const passwordInput = document.querySelector('#password-signup') as HTMLInputElement;
     const steamIdInput = document.querySelector('#steamId-signup') as HTMLInputElement;
 
-
+ // Trimming the values to remove any leading/trailing whitespace.
     const fullName = nameInput.value.trim();
     const email = emailInput.value.trim();
     const password = passwordInput.value.trim();
@@ -77,7 +80,7 @@ const signupFormHandler = async (event: Event): Promise<void> => {
     }
 };
 
-
+// Adding an event listener to the registration form for the submit event.
 if (registerForm) {
     registerForm.addEventListener('submit', signupFormHandler);
 }
